ebbf0a1f2b30019698d660ba629493d3d7957bf1
BoomRedeemer <59974071+BoomRedeemer@users.noreply.github.com>
2020-10-02 19:50:43 +0300

    Считывание файла или файлов из папки по аргументам

--- LogParser/Program.cs
+++ LogParser/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace LogParser
 {
@@ -6,7 +7,46 @@ namespace LogParser
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string sourceDirectory = "";
+            Console.WriteLine("Введите путь к папке");
+           sourceDirectory = Console.ReadLine();
+            try
+           {
+                var txtFiles = Directory.EnumerateFiles(sourceDirectory, "*.txt");
+                foreach (string currentFile in txtFiles)
+                {
+                    string fileName = currentFile.Substring(sourceDirectory.Length + 1);
+                    Console.WriteLine(fileName);
+                StreamReader sr = new StreamReader(currentFile);
+
+                string s;
+
+                while (sr.EndOfStream != true)
+                {
+                    s = sr.ReadLine();
+                    Console.WriteLine(s);
+                }
+                sr.Close();       
+            }
+          }
+            catch (Exception e)
+            {
+                try
+                {
+                    using StreamReader sr = new StreamReader(sourceDirectory);
+                    string line;
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                      Console.WriteLine(line);
+                  }
+               }
+               catch(Exception c)
+              {
+                    Console.WriteLine("такого пути или файла нет");
+                }
+          }
+          }
+        
         }
     }
-}
+
